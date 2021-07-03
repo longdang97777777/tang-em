5fc50b4a1b8c368290ce29088cc419666d04b51f
longdang97777777 <86861334+longdang97777777@users.noreply.github.com>
2021-07-03 09:06:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Xù à!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Nhớ live thường xuyên nhé <3<3<3.',
+    messDesc: 'Tối a đón đi chơi nhé <3<3<3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/dinhnam246' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/philong.dang.90' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
